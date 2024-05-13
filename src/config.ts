1f9f64b69ba53a5c5111c98f54374ed467379c4a
shiki0706 <minjun.huang@outlook.com>
2024-05-13 23:48:22 +0800

    update

--- src/config.ts
+++ src/config.ts
@@ -8,7 +8,6 @@ import { LinkPreset } from './types/config'
 
 export const siteConfig: SiteConfig = {
   title: '林深见鹿',
-  subtitle: '',
   lang: 'zh_CN',         // 'en', 'zh_CN', 'zh_TW', 'ja'
   themeColor: {
     hue: 250,         // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
@@ -45,24 +44,7 @@ export const profileConfig: ProfileConfig = {
   name: 'Shiki',
   bio: '自在飞花轻似梦',
   links: [
-    {
-      name: 'Twitter',
-      icon: 'fa6-brands:twitter',       // Visit https://icones.js.org/ for icon codes
-                                        // You will need to install the corresponding icon set if it's not already included
-                                        // `pnpm add @iconify-json/<icon-set-name>`
-      url: 'https://twitter.com',
-    },
-    {
-      name: 'Steam',
-      icon: 'fa6-brands:steam',
-      url: 'https://store.steampowered.com',
-    },
-    {
-      name: 'GitHub',
-      icon: 'fa6-brands:github',
-      url: 'https://github.com/saicaca/fuwari',
-    },
-  ],
+          ],
 }
 
 export const licenseConfig: LicenseConfig = {
