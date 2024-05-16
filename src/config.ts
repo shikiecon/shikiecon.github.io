387d68c0257f2570aa2ef01848aa77a03bd70b6a
shiki0706 <minjun.huang@outlook.com>
2024-05-16 20:20:24 +0800

    update

--- src/config.ts
+++ src/config.ts
@@ -8,7 +8,7 @@ import { LinkPreset } from './types/config'
 
 export const siteConfig: SiteConfig = {
   title: '林深见鹿',
-  subtitle: '',
+  subtitle: '随心所欲',
   lang: 'zh_CN',         // 'en', 'zh_CN', 'zh_TW', 'ja'
   themeColor: {
     hue: 250,         // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
